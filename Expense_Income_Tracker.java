8a6f36082f7efaaaa21d95adeeefcd80cc26ce3d
yashshindeact <yashshindeact@gmail.com>
2025-02-25 21:45:55 +0530

    Create Expense_Income_Tracker.java

---
+++ Expense_Income_Tracker.java
@@ -0,0 +1,7 @@
+package expense_income_tracker;
+
+public class Expense_Income_Tracker {
+    public static void main(String[] args) {
+        new ExpensesIncomesTracker().setLocationRelativeTo(null);
+    }
+}
